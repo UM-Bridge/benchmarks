80533051915555b01a5659adbbb465a1b035d74e
reinarz <anne.k.reinarz@durham.ac.uk>
2022-04-06 13:39:18 +0100

    add autodocs extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../benchmarks/'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'UM-Bridge Benchmarks'
+project = 'benchmarks'
 copyright = '2022, UM-Bridge Team'
 author = 'UM-Bridge Team'
 
@@ -28,6 +28,7 @@ author = 'UM-Bridge Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -37,7 +38,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -56,4 +56,3 @@ html_static_path = ['_static']
 extensions = ['myst_parser']
 
 source_suffix = ['.rst', '.md']
-
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,10 +1,10 @@
-.. UM-Bridge Benchmarks documentation master file, created by
-   sphinx-quickstart on Wed Apr  6 11:01:31 2022.
+.. benchmarks documentation master file, created by
+   sphinx-quickstart on Wed Apr  6 13:34:13 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to UM-Bridge Benchmarks's documentation!
-================================================
+Welcome to benchmarks's documentation!
+======================================
 
 .. toctree::
    :maxdepth: 2
