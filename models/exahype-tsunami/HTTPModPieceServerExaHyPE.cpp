e701cd624e8a0f567aa4a6d02c44185eba1f7a13
reinarz <anne.k.reinarz@durham.ac.uk>
2022-04-20 12:06:04 +0100

    move model from mpitest branch over

--- models/exahype-tsunami/HTTPModPieceServerExaHyPE.cpp
+++ models/exahype-tsunami/HTTPModPieceServerExaHyPE.cpp
@@ -2,20 +2,20 @@
 
 #include <string>
 
-#include "umbridge.h"
+//#include <resolv.h> // Header included in httplib.h, causing potential issues with Eigen!
+
+#include "HTTPComm.h"
 
 #include <chrono>
 #include <thread>
 #include <iomanip>
 #include <stdlib.h>
 
-int test_delay = 0;
-
-class ExampleModel : public umbridge::Model {
+class ExampleModPiece : public ShallowModPiece {
 public:
 
-  ExampleModel()
-   : umbridge::Model(Eigen::VectorXi::Ones(1)*2, Eigen::VectorXi::Ones(1)*4)
+  ExampleModPiece(int ranks)
+   : ShallowModPiece(Eigen::VectorXi::Ones(1)*2, Eigen::VectorXi::Ones(1)*4), ranks(ranks)
   {
     outputs.push_back(Eigen::VectorXd::Ones(4));
 
@@ -28,6 +28,7 @@ public:
   }
 
   void Evaluate(std::vector<std::reference_wrapper<const Eigen::VectorXd>> const& inputs, json config) override {
+    std::cout << "Reading options" << std::endl;
     int level = config.value("level", 0);
     bool verbose = config.value("verbosity", false);
     bool vtk_output = config.value("vtk_output", false);
@@ -62,22 +63,33 @@ public:
         system("cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"time\": 1.0,/\"time\": 10000.0,/g' SWE_asagi_limited_l2.exahype2");
     }
     if(level == 0) {
-      status = system("cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l0 && ./ExaHyPE-SWE ../SWE_asagi_limited_l0.exahype2");
+      std::string cmd = "cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l0 && mpirun --allow-run-as-root -x LD_LIBRARY_PATH -x SHARED_DIR -n " + std::to_string(ranks) + " ./ExaHyPE-SWE ../SWE_asagi_limited_l0.exahype2";
+      status = system(cmd.c_str());
     } else if(level == 1) {
-      status = system("cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l1 && ./ExaHyPE-SWE ../SWE_asagi_limited_l1.exahype2");
+      std::string cmd = "cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l1 && mpirun --allow-run-as-root -x LD_LIBRARY_PATH -x SHARED_DIR -n " + std::to_string(ranks) + " ./ExaHyPE-SWE ../SWE_asagi_limited_l1.exahype2";
+      status = system(cmd.c_str());
     } else if(level == 2) {
-      status = system("cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l2 && ./ExaHyPE-SWE ../SWE_asagi_limited_l2.exahype2");
+      std::string cmd = "cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l2 && mpirun --allow-run-as-root -x LD_LIBRARY_PATH -x SHARED_DIR -n " + std::to_string(ranks) + " ./ExaHyPE-SWE ../SWE_asagi_limited_l2.exahype2";
+      status = system(cmd.c_str());
     } else {
       std::cerr << "Unknown model requested by client!" << std::endl;
       exit(-1);
     }
     std::cout << "Exahype exit status " << status << std::endl;
-
-    std::ifstream outputsfile(shared_dir + "outputs.txt");
-    for (int i = 0; i < outputs[0].rows(); i++) {
+    {
+    std::ifstream outputsfile(shared_dir + "Probe18outputs.txt");
+    for (int i = 0; i < 2; i++) {
       outputsfile >> outputs[0](i);
     }
     outputsfile.close();
+    }
+    {
+    std::ifstream outputsfile(shared_dir + "Probe19outputs.txt");
+    for (int i = 2; i < 4; i++) {
+      outputsfile >> outputs[0](i);
+    }
+    outputsfile.close();
+    }
     std::cout << "Read outputs from exahype:" << outputs[0] << std::endl;
 
     std::cout << "Left" << std::endl;
@@ -87,6 +99,7 @@ public:
     return true;
   }
 private:
+  int ranks;
   std::string shared_dir;
 };
 
@@ -97,17 +110,18 @@ int main(){
     std::cerr << "Environment variable PORT not set!" << std::endl;
     exit(-1);
   }
-
-  char const* delay_cstr = std::getenv("TEST_DELAY");
-  if ( delay_cstr != NULL ) {
-    test_delay = atoi(delay_cstr);
-  }
-
-
   const int port = atoi(port_cstr);
-  ExampleModel model;
 
-  umbridge::serveModel(model, "0.0.0.0", port);
+  char const* ranks_cstr =  std::getenv("RANKS");
+  if ( ranks_cstr == NULL ) {
+    std::cerr << "Environment variable RANKS not set!" << std::endl;
+    exit(-1);
+  }
+  const int ranks = atoi(ranks_cstr);
+
+  ExampleModPiece modPiece(ranks);
+
+  serveModPiece(modPiece, "0.0.0.0", port);
 
   return 0;
 }
