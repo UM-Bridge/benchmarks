fdfa2bde993966050d48179bb2160ec567c29bf1
reinarz <anne.k.reinarz@durham.ac.uk>
2022-04-23 12:21:41 +0100

    generate rst files automatically

---
+++ docs/generate_rst_files_from_readmes.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+# Loop over all benchmarks
+for f in $(find ../benchmarks/ -name 'README.md'); do
+    NAME=`echo $f | xargs dirname | xargs basename`
+    echo "Name of generated file " $NAME.rst
+    #rm source/benchmarks/$NAME.rst
+    cat > source/benchmarks/$NAME.rst << EOF
+.. include::../$f
+:parser: myst_parser.sphinx_
+EOF
+done
+
+# Loop over all models
+for f in $(find ../models/ -name 'README.md'); do
+    NAME=`echo $f | xargs dirname | xargs basename`
+    echo "Name of generated file " $NAME.rst
+    #rm source/models/$NAME.rst
+    cat > source/models/$NAME.rst << EOF
+.. include::../$f
+:parser: myst_parser.sphinx_
+EOF
+done
