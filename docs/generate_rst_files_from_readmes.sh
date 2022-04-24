68fd3969a0e7e0f8ab1e8ab618579de2c99ed8a5
reinarz <anne.k.reinarz@durham.ac.uk>
2022-04-24 11:35:33 +0100

    generate rst files automatically

--- docs/generate_rst_files_from_readmes.sh
+++ docs/generate_rst_files_from_readmes.sh
@@ -1,23 +1,46 @@
 #/bin/bash
 
+# Delete older versions
+rm source/benchmarks/*
+rm source/models/*
+
+# Create index file
+cat > source/benchmarks/index.rst << EOF
+.. include:: ../../../benchmarks/README.md
+   :parser: myst_parser.sphinx_
+
+For more details on the benchmarks see their individual documentation:
+
+.. toctree::
+   :maxdepth: 3
+
+EOF
+
+
 # Loop over all benchmarks
 for f in $(find ../benchmarks/ -name 'README.md'); do
     NAME=`echo $f | xargs dirname | xargs basename`
-    [ ! -z "$NAME" ] || NAME=index
+    [ "$NAME" != "benchmarks" ] || continue
     echo "Name of generated file " $NAME.rst
     cat > source/benchmarks/$NAME.rst << EOF
-.. include::../$f
-:parser: myst_parser.sphinx_
+.. include:: ../../$f
+   :parser: myst_parser.sphinx_
+
+EOF
+    # Append to toctree
+    cat >> source/benchmarks/index.rst << EOF
+   $NAME
 EOF
 done
 
 # Loop over all models
 for f in $(find ../models/ -name 'README.md'); do
     NAME=`echo $f | xargs dirname | xargs basename`
-    [ ! -z "$NAME" ] || NAME=index
+    [ "$NAME" != "models" ] || continue
     echo "Name of generated file " $NAME.rst
     cat > source/models/$NAME.rst << EOF
-.. include:: ../$f
-    :parser: myst_parser.sphinx_
+.. include:: ../../$f
+   :parser: myst_parser.sphinx_
+
 EOF
 done
