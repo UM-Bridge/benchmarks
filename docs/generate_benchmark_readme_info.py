e25d460740eb1883622bd9004b8908fbf52a1f51
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-09-25 15:07:32 +0200

    Update model docs generator

--- docs/generate_benchmark_readme_info.py
+++ docs/generate_benchmark_readme_info.py
@@ -1,6 +1,12 @@
+import argparse
 import umbridge
 
-model = umbridge.HTTPModel("http://localhost:4242")
+# Read URL from command line argument
+parser = argparse.ArgumentParser(description='Minimal HTTP model demo.')
+parser.add_argument('url', metavar='url', type=str,
+                    help='the ULR on which the model is running, for example http://localhost:4242')
+args = parser.parse_args()
+print(f"Connecting to URL {args.url}")
 
 print("# Model / Benchmark Name")
 
@@ -10,30 +16,43 @@ print("## Authors")
 
 print("## Run")
 
-print("## Properties")
+print("## Model properties")
 print("")
 
-print("Mapping | Dimensions | Description")
-print("---|---|---")
-print(f"input | {model.get_input_sizes()} | TODO: INPUT DESCRIPTION")
-print(f"output | {model.get_output_sizes()} | TODO: OUTPUT DESCRIPTION")
-
-print("")
-
-print("Feature | Supported")
+print("Model | Description")
 print("---|---")
-print(f"Evaluate | {model.supports_evaluate()}")
-print(f"Gradient | {model.supports_gradient()}")
-print(f"ApplyJacobian | {model.supports_apply_jacobian()}")
-print(f"ApplyHessian | {model.supports_apply_hessian()}")
-
+for model_name in umbridge.supported_models(args.url):
+  print(f"{model_name} | TODO: Description")
 print("")
 
-print("Config | Type | Default | Description")
-print("---|---|---|---")
-print("None | | |")
+for model_name in umbridge.supported_models(args.url):
+  model = umbridge.HTTPModel(args.url, model_name)
 
-print("")
+  print(f"### {model_name}")
+
+  print(f"Mapping | Dimensions | Description")
+  print("---|---|---")
+  print(f"input | {model.get_input_sizes()} | TODO: INPUT DESCRIPTION")
+  print(f"output | {model.get_output_sizes()} | TODO: OUTPUT DESCRIPTION")
+
+  print("")
+
+  print("Feature | Supported")
+  print("---|---")
+  print(f"Evaluate | {model.supports_evaluate()}")
+  print(f"Gradient | {model.supports_gradient()}")
+  print(f"ApplyJacobian | {model.supports_apply_jacobian()}")
+  print(f"ApplyHessian | {model.supports_apply_hessian()}")
+
+  print("")
+
+  print("Config | Type | Default | Description")
+  print("---|---|---|---")
+  print("None | | |")
+
+  print("")
+
+print("## Mount directories")
 
 print("Mount directory | Purpose")
 print("---|---")
