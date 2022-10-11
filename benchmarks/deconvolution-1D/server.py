98260d5276285f95055fda1bd8aeeaf7201c331f
nabr <nabr@dtu.dk>
2022-10-11 23:12:26 +0200

    Add models for different choices of prior

--- benchmarks/deconvolution-1D/server.py
+++ benchmarks/deconvolution-1D/server.py
@@ -3,13 +3,26 @@ import numpy as np
 import cuqi
 
 class Deconvolution1D(umbridge.Model):
-    dim = 128
+    """ Base benchmark for all 1D deconvolution problems """
 
-    def __init__(self):
-        # Load test problem (posterior + data)
-        np.random.seed(0) # Fix seed for now. Can maybe store data instead and provide it to test problem
-        self.TP = cuqi.testproblem.Deconvolution1D(dim=self.dim)
-        super().__init__(self.__class__.__name__)
+    dim = 128 # Dimension of the 1D signal
+
+    def __init__(self, name):
+        """ Initialize the model
+        
+        parameters
+        ----------
+        name : str
+            Name of the model
+
+         """
+        # Load data from file
+        data = np.load('data_pc.npz')
+
+        # Set up test problem
+        self.TP = cuqi.testproblem.Deconvolution1D(dim=self.dim, phantom='pc', data = data['data'])
+
+        super().__init__(name)
 
     def get_input_sizes(self, config):
         return [self.dim]
@@ -30,7 +43,58 @@ class Deconvolution1D(umbridge.Model):
 
     def supports_gradient(self):
         return True
+    
+class Deconvolution1D_Gaussian(Deconvolution1D):
+    """ Deconvolution 1D with Gaussian prior """
+    def __init__(self):
+        super().__init__(self.__class__.__name__)
+        self.TP.prior = cuqi.distribution.GaussianCov(np.zeros(self.dim), 0.1)
 
-model = Deconvolution1D()
+class Deconvolution1D_GMRF(Deconvolution1D):
+    """ Deconvolution 1D with Gaussian Markov Random Field (GMRF) prior """
+    def __init__(self):
+        super().__init__(self.__class__.__name__)
+        self.TP.prior = cuqi.distribution.GMRF(np.zeros(self.dim), 10)
 
-umbridge.serve_models([model], 4243)
+class Deconvolution1D_LMRF(Deconvolution1D):
+    """ Deconvolution 1D with Laplace Markov Random Field (LMRF) prior """
+    def __init__(self):
+        super().__init__(self.__class__.__name__)
+        self.TP.prior = cuqi.distribution.Laplace_diff(np.zeros(self.dim), 0.01)
+
+    def supports_gradient(self):
+        return False
+
+class Deconvolution1D_CMRF(Deconvolution1D):
+    """ Deconvolution 1D with Cauchy Markov Random Field (CMRF) prior """
+    def __init__(self):
+        super().__init__(self.__class__.__name__)
+        self.TP.prior = cuqi.distribution.Cauchy_diff(np.zeros(self.dim), 0.01)
+
+class ExactSolution(umbridge.Model):
+    """ Exact solution for the 1D deconvolution problem """
+    def __init__(self):
+        """ Initialize the model. Load the exact solution from file. """
+        data = np.load('data_pc.npz')
+        self.exactSolution = data['exact']
+        super().__init__(self.__class__.__name__)
+
+    def get_input_sizes(self, config):
+        return [0]
+
+    def get_output_sizes(self, config):
+        return [len(self.exactSolution)]
+
+    def __call__(self, parameters, config):
+        return [[self.exactSolution.tolist()]]
+
+    def supports_evaluate(self):
+        return True
+
+model_Gaussian = Deconvolution1D_Gaussian()
+model_GMRF = Deconvolution1D_GMRF()
+model_LMRF = Deconvolution1D_LMRF()
+model_CMRF = Deconvolution1D_CMRF()
+model_exactSolution = ExactSolution()
+
+umbridge.serve_models([model_Gaussian, model_GMRF, model_LMRF, model_CMRF, model_exactSolution], 4243)
