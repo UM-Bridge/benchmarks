2ebd2346e3e2648280c93c689df9fa8469dd65b9
nabr <nabr@dtu.dk>
2022-10-13 22:49:14 +0200

    Add full testing + reg test of output

--- benchmarks/deconvolution-1D/test_output.py
+++ benchmarks/deconvolution-1D/test_output.py
@@ -1,36 +1,93 @@
 #!/usr/bin/env python3
+""" Script to regression test the output of the deconvolution-1D benchmark. """
+
 import argparse
 import umbridge
 import pytest
 import cuqi
 import numpy as np
 
+# Set up CUQIpy testproblem to compare with
+print('Setting up testproblem')
+data = np.load("data_pc.npz")
+TP = cuqi.testproblem.Deconvolution1D(dim=128, phantom="pc", data=data["data"])
+parameters = TP.exactSolution # Extract test parameters to evaluate on
+
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='Model output test.')
 parser.add_argument('url', metavar='url', type=str,
                     help='the ULR on which the model is running, for example http://localhost:4242')
 args = parser.parse_args()
 print(f"Connecting to host URL {args.url}")
 
-model = umbridge.HTTPModel(args.url, "posterior")
+# Define models for each type of prior
+print('Setting up models')
+model_Gaussian = umbridge.HTTPModel(args.url, "Deconvolution1D_Gaussian")
+model_GMRF = umbridge.HTTPModel(args.url, "Deconvolution1D_GMRF")
+model_CMRF = umbridge.HTTPModel(args.url, "Deconvolution1D_CMRF")
+model_LMRF = umbridge.HTTPModel(args.url, "Deconvolution1D_LMRF")
 
-# Load test problem (same seed as in server.py)
-np.random.seed(0)
-TP = cuqi.testproblem.Deconvolution1D(dim=128)
+# Model for exact solution
+model_exactSolution = umbridge.HTTPModel(args.url, "ExactSolution")
 
-# Extract test parameters to evaluate on
-parameters = TP.exactSolution
+# Check basic model properties
+print('Checking model properties')
+assert model_Gaussian.get_input_sizes() == [128]
+assert model_GMRF.get_input_sizes() == [128]
+assert model_CMRF.get_input_sizes() == [128]
+assert model_LMRF.get_input_sizes() == [128]
+assert model_exactSolution.get_input_sizes() == [0]
 
-# Test Info
-print(f"Model input and output: {model.get_input_sizes()}, {model.get_output_sizes()}")
+assert model_Gaussian.get_output_sizes() == [1]
+assert model_GMRF.get_output_sizes() == [1]
+assert model_CMRF.get_output_sizes() == [1]
+assert model_LMRF.get_output_sizes() == [1]
+assert model_exactSolution.get_output_sizes() == [128]
 
-# Supports evaluate?
-print(f"Model supports evaluate: {model.supports_evaluate()}")
+# Check that the models support evaluation and gradient
+print('Checking model capabilities')
+assert model_Gaussian.supports_evaluate()
+assert model_GMRF.supports_evaluate()
+assert model_CMRF.supports_evaluate()
+assert model_LMRF.supports_evaluate()
+assert model_exactSolution.supports_evaluate()
 
-# Supports gradient?
-print(f"Model supports gradient: {model.supports_gradient()}")
+assert model_Gaussian.supports_gradient()
+assert model_GMRF.supports_gradient()
+assert model_CMRF.supports_gradient()
+assert not model_LMRF.supports_gradient()
+assert not model_exactSolution.supports_gradient()
 
-# Test evaluate
-print("Evaluating model...")
-print(model([parameters.tolist()]) == pytest.approx(TP.posterior.logpdf(parameters)))
-print("Finished evaluating model.")
+# Evaluate models and compare with testproblem
+print('Evaluating models')
+output_Gaussian = model_Gaussian([parameters.tolist()])[0][0]
+output_GMRF = model_GMRF([parameters.tolist()])[0][0]
+output_CMRF = model_CMRF([parameters.tolist()])[0][0]
+output_LMRF = model_LMRF([parameters.tolist()])[0][0]
+output_exactSolution = model_exactSolution([[]])[0]
+
+# Check that the output is correct
+print('Checking model output')
+TP.prior = cuqi.distribution.GaussianCov(np.zeros(128), 0.1)
+assert output_Gaussian == pytest.approx(TP.posterior.logpdf(parameters))
+
+TP.prior = cuqi.distribution.GMRF(np.zeros(128), 10)
+assert output_GMRF == pytest.approx(TP.posterior.logpdf(parameters))
+
+TP.prior = cuqi.distribution.Cauchy_diff(np.zeros(128), 0.01)
+assert output_CMRF == pytest.approx(TP.posterior.logpdf(parameters))
+
+TP.prior = cuqi.distribution.Laplace_diff(np.zeros(128), 0.01)
+assert output_LMRF == pytest.approx(TP.posterior.logpdf(parameters))
+
+assert np.allclose(output_exactSolution, TP.exactSolution)
+
+print('Regression testing against known norm values of output')
+assert np.linalg.norm(output_Gaussian) == pytest.approx(492.6685962829914)
+assert np.linalg.norm(output_GMRF) == pytest.approx(169.7613099191894)
+assert np.linalg.norm(output_CMRF) == pytest.approx(585.9518078000048)
+assert np.linalg.norm(output_LMRF) == pytest.approx(97.75894228418153)
+assert np.linalg.norm(output_exactSolution) == pytest.approx(12.0)
+
+print('All tests passed')
 
