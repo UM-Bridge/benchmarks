7687cf74cc0dbbe31f09052cb4e8b7b1478f7e3f
Katherine Rosenfeld <krosenf@gmail.com>
2023-02-17 13:03:42 -0800

    update README and server

--- benchmarks/abm-attack-fraction/server.py
+++ benchmarks/abm-attack-fraction/server.py
@@ -17,11 +17,15 @@ class EMODForwardModel(umbridge.Model):
     Todo:
         * check that running in parallel does not have a conflict.
     '''
+    # next_id = 0
 
     def __init__(self):
         super().__init__("forward")
         self.run_number = 0
         # np.random.default_rng(42)
+        # self.run_number = EMODForwardModel.next_id
+        # EMODForwardModel.next_id += 1
+
 
     def get_input_sizes(self, config):
         return [3]
@@ -33,52 +37,70 @@ class EMODForwardModel(umbridge.Model):
     def __call__(self, parameters, config):
         '''
         Args:
-            parameters (list): [[R0], [R0 variance], [Acquisition Correlation]]
-            config (dict): {}
+            # parameters (list): [[R0], [R0 variance], [Acquisition Correlation]]
+            parameters (list): [[R0], [Acquisition Correlation]]
+            config (dict): {refresh_seed:True, daily_import_pressures:1.0}
 
         Returns:
-            list: [[attack fraction]]
+            list: [[attack fraction]]            
         '''
+
+        # extract config
+        refresh_seed = config.get('refresh_seed', True)
+        daily_import_pressures = config.get('daily_import_pressures', 1.0)
+
+        # unpack parameters
         self.run_number += 1
         model_id = deepcopy(self.run_number)
-        # model_id = 1
         R0 = parameters[0][0]
         R0_VAR = parameters[0][1]
         Acquisition_Transmission_Correlation = parameters[0][2]
 
-        config = json.load(open('config.json', 'r'))
+        # load configuration and campaign files
+        config_dict = json.load(open('config.json', 'r'))
+        campaign_dict = json.load(open('campaign.json', 'r'))
 
         # check bounds
         if (R0_VAR <= 0) or (R0 <= 0) or  \
             (Acquisition_Transmission_Correlation > 1) or \
                 (Acquisition_Transmission_Correlation < 0):
-            # json.dump(config, open('config_{:0d}.json'.format(model_id), 'w'))
-            # self._cleanup()
-            return [[-np.inf]]
+            return [[-999]]
 
+        # update config file
         inf_prd_mean   = 8.0
         inf_ln_mean    = R0/inf_prd_mean
         inf_ln_var     = R0_VAR/inf_prd_mean/inf_prd_mean
         inf_ln_sig     = np.sqrt(np.log(inf_ln_var/inf_ln_mean/inf_ln_mean+1.0))
         inf_ln_mu      = np.log(inf_ln_mean) - 0.5*inf_ln_sig*inf_ln_sig
+        if refresh_seed:
+            config_dict['parameters']['Run_Number'] = int(model_id)
+        config_dict['parameters']['Base_Infectivity_Log_Normal_Mu'] = inf_ln_mu
+        config_dict['parameters']['Base_Infectivity_Log_Normal_Sigma'] = inf_ln_sig
+        config_dict['parameters']['Acquisition_Transmission_Correlation'] = Acquisition_Transmission_Correlation
+        config_dict['parameters']['Infectious_Period_Gaussian_Mean'] = inf_prd_mean
+        
+        # update campaign file
+        campaign_dict['Events'][0]['Event_Coordinator_Config']['Intervention_Config']['Daily_Import_Pressures'] = [daily_import_pressures]
 
-        # load the template json config file (not thread safe)
-        config['parameters']['Run_Number'] = int(model_id)
-        config['parameters']['Base_Infectivity_Log_Normal_Mu'] = inf_ln_mu
-        config['parameters']['Base_Infectivity_Log_Normal_Sigma'] = inf_ln_sig
-        config['parameters']['Acquisition_Transmission_Correlation'] = Acquisition_Transmission_Correlation
-        config['parameters']['Infectious_Period_Gaussian_Mean'] = inf_prd_mean
-        json.dump(config, open('config_{:0d}.json'.format(model_id), 'w'))
+        # set up campaign, config, and dempographics fils
+        model_directory = f"input_{model_id:0d}"
+        if not os.path.exists(model_directory):
+            os.makedirs(model_directory)        
+        json.dump(config_dict, open(os.path.join(model_directory, 'config.json'), 'w'))
+        json.dump(campaign_dict, open(os.path.join(model_directory, 'campaign.json'), 'w'))
+        cmd = f"cp demographics.json {model_directory}"
+        os.system(cmd)
 
         # run EMOD
-        cmd = '/EMOD/Eradication -C config_{:0d}.json'.format(model_id)
+        cmd = f"/EMOD/Eradication -C {model_directory}/config.json -I {model_directory} -O output_{model_id:0d}"
         os.system(cmd)
 
         # read in result
-        inset_chart = json.load(open(os.path.join('output', 'InsetChart.json'), 'r'))
+        inset_chart = json.load(open(os.path.join(f"output_{model_id:0d}", 'InsetChart.json'), 'r'))
 
         # attack fraction = 1 - susceptible population
         model_output = 1 - inset_chart['Channels']['Susceptible Population']['Data'][-1]
+        print(model_output)
 
         self._cleanup()
 
@@ -88,15 +110,20 @@ class EMODForwardModel(umbridge.Model):
         '''
         Cleanup generated .json file
         '''
-        # model_id = deepcopy(self.run_number)
-        cmd = 'rm config_{:0d}.json'.format(self.run_number)
+        directory = f"input_{self.run_number:0d}"
+        cmd = f"rm -rf {directory}"
         os.system(cmd)
+
+        directory = f"output_{self.run_number:0d}"
+        cmd = f"rm -rf {directory}"
+        os.system(cmd)        
 
     def supports_evaluate(self):
         return True    
 
 
 class EMODBenchmarkModel(umbridge.Model):
+
     def __init__(self):
         super().__init__("posterior")
         self.model = EMODForwardModel()
@@ -111,12 +138,12 @@ class EMODBenchmarkModel(umbridge.Model):
 
         model_output = self.model(parameters, config)[0]
         # enforce boundary
-        if not np.isfinite(model_output[0]):
-            return [[-1e30]]
+        if model_output[0] < 0:
+            return [[-1e60]]
 
         # likelihood definition
         data = [0.4]
-        likelihood_std_dev = [0.1]
+        likelihood_std_dev = [0.025]
         likelihood_cov_matrix_diag = [likelihood_std_dev[0]**2]
 
         posterior = scipy.stats.multivariate_normal.logpdf(model_output, data, likelihood_cov_matrix_diag)
