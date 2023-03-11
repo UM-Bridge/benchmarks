688a7491b08609db1663668554132f5382df58e5
Linus Seelinger <mail@linusseelinger.de>
2023-03-11 11:18:57 +0100

    Update l2-sea

--- models/l2-sea/umbridge-server.py
+++ models/l2-sea/umbridge-server.py
@@ -8,7 +8,7 @@ class L2Sea(umbridge.Model):
         super().__init__("forward")
 
     def get_input_sizes(self, config):
-        return [15]
+        return [16]
 
     def get_output_sizes(self, config):
         return [5]
@@ -16,7 +16,7 @@ class L2Sea(umbridge.Model):
     def __call__(self, parameters, config):
         # Write first (and only) parameter vector to file
         with open('/NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415/variables.inp', 'w') as f:
-            for param in parameters[0][1:]:
+            for param in parameters[0][2:]:
                 f.write(str(param) + '\n')
             f.close()
 
@@ -27,17 +27,20 @@ class L2Sea(umbridge.Model):
         with open('/NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415/SBDF.nml') as nml_file:
             nml = f90nml.read(nml_file)
 
-        nml['MAIN_PARAMETERS']['igrid'] = config.get("fidelity", 7)
+        nml['MAIN_PARAMETERS']['igrid'] = config.get("fidelity")
         nml['FREE_WARP']['fr'] = parameters[0][0]
-        nml['FREE_WARP']['sinkoff'] = config.get("sinkoff", 'y')
-        nml['FREE_WARP']['trimoff'] = config.get("trimoff", 'y')
+        nml['FREE_WARP']['sinkoff'] = config.get("sinkoff")
+        nml['FREE_WARP']['trimoff'] = config.get("trimoff")
+        nml['PANCA_PARAMETERS']['ztrasla'] = parameters[0][1]
+
         nml.write('/NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415/SBDF.aux')
 
         # System call, cd into working directory and call model binary
         print(config)
-        return_value = os.system('cd /NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415 && mv SBDF.aux SBDF.nml && ../../bin/L2-Sea')
+        return_value = os.system('cd /NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415; mv SBDF.aux SBDF.nml; ../../bin/L2-Sea')
         if return_value != 0:
             return [[0,0,0,0,0]]
+
 
         # Read second line of output file, split and return last 5 elements as output
         with open('/NATO-AVT-331-L2-Sea-Benchmark/examples/DTMB-5415/CPU000/objective.out', 'r') as f:
