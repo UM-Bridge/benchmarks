Fix gradient in cuqi-ct

benchmarks/cuqi-ct/server.py
@@ -92,8 +92,8 @@ class CT_UM(umbridge.Model):
 
     def gradient(self, out_wrt, in_wrt, parameters, sens, config):
         posterior = self._configure_posterior(config)
-        output = posterior.gradient(np.asarray(parameters[0]))
-        return [output.tolist()]
+        output = posterior.gradient(np.asarray(parameters[0])) * sens
+        return output.tolist()
 
     def supports_evaluate(self):
         return True