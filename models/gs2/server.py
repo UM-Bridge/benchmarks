f8b82e27fb17a471142ae0f6beeb4e02ea465fdb
chun9l <97897047+chun9l@users.noreply.github.com>
2023-11-20 12:50:19 +0000

    Runs with multiple inputs and will modify input file

--- models/gs2/server.py
+++ models/gs2/server.py
@@ -2,225 +2,61 @@ import umbridge
 import json
 import os
 import csv
+from pyrokinetics import Pyro
+import numpy as np
+from datetime import datetime
+from fileinput import FileInput
+import shlex
 
-class AchlysModel(umbridge.Model):
+
+
+class GS2Model(umbridge.Model):
     def __init__(self):
         super().__init__("forward")
 
     def get_input_sizes(self, config):
-        return [5] # Needs changing
+        return [2] 
 
     def get_output_sizes(self, config):
-        return [500] # Needs changing
+        return [3] 
 
     def __call__(self, parameters, config):
-
-        input = f"""
-            {{
-                &kt_grids_knobs
-                    grid_option = 'single'
-                /
-
-                &kt_grids_single_parameters
-                    aky = 0.7852773972394589
-                    theta0 = 0.0
-                /
-
-                &theta_grid_parameters
-                    ntheta = 128
-                    nperiod = 9
-                    akappa = 2.66
-                    akappri = -0.25
-                    rhoc = 0.67
-                    tri = 0.3400000000000001
-                    tripri = 0.25
-                    shat = 0.87
-                    qinp = 8.903370045724728
-                    r_geo = 1.84
-                    rmaj = 1.84
-                    shift = -0.44
-                    geotype = 0
-                /
-
-                &theta_grid_knobs
-                    equilibrium_option = 'eik'
-                /
-
-                &theta_grid_eik_knobs
-                    irho = 2
-                    iflux = 0
-                    s_hat_input = 3.1173933836235315
-                    beta_prime_input = -0.64
-                    local_eq = .true.
-                    bishop = 4
-                    equal_arc = .false.
-                    writelots = .true.
-                /
-
-                &le_grids_knobs
-                    ngauss = 8
-                    negrid = 16
-                    bouncefuzz = 1e-08
-                /
-
-                &dist_fn_knobs
-                    adiabatic_option = 'iphi00=2'
-                    opt_source = .true.
-                    def_parity = .true.
-                    even = .false.
-                /
-
-                &fields_knobs
-                    field_option = 'implicit'
-                    response_dir = 'response'
-                /
-
-                &knobs
-                    fphi = 1.0
-                    fapar = 1.0
-                    fbpar = 0.0
-                    delt = 0.02
-                    nstep = 80000
-                    wstar_units = .true.
-                /
-
-                &layouts_knobs
-                    layout = 'xyles'
-                /
-
-                &collisions_knobs
-                    collision_model = 'default'
-                /
-
-                &species_knobs
-                    nspec = 3
-                /
-
-                &species_parameters_1
-                    z = 1.0
-                    mass = 1.0
-                    dens = 0.5
-                    temp = 1.0593220338983051
-                    tprim = 0.0
-                    fprim = 0.1450502618121151
-                    uprim = 0.0
-                    vnewk = 0.00039999999999999996
-                    type = 'ion'
-                    bess_fac = 1.0
-                /
-
-                &dist_fn_species_knobs_1
-                    fexpr = 0.48
-                    bakdif = 0.05
-                /
-
-                &species_parameters_2
-                    z = 1.0
-                    mass = 1.4975
-                    dens = 0.5
-                    temp = 1.0593220338983051
-                    tprim = 0.0
-                    fprim = 0.1450502618121151
-                    uprim = 0.0
-                    vnewk = 0.0003268711385781747
-                    type = 'ion'
-                    bess_fac = 1.0
-                /
-
-                &dist_fn_species_knobs_2
-                    fexpr = 0.48
-                    bakdif = 0.05
-                /
-
-                &species_parameters_3
-                    z = -1
-                    mass = 0.0002724
-                    dens = 1.0
-                    temp = 1.0
-                    tprim = 4.98536245337014
-                    fprim = 2.0329154559480602
-                    uprim = 0.0
-                    vnewk = 0.011195269013375201
-                    type = 'electron'
-                    bess_fac = 1.0
-                /
-
-                &dist_fn_species_knobs_3
-                    fexpr = 0.48
-                    bakdif = 0.05
-                /
-
-                &init_g_knobs
-                    ginit_option = 'default_odd'
-                    chop_side = .false.
-                    phiinit = 1e-05
-                    restart_dir = 'restart'
-                /
-
-                &gs2_diagnostics_knobs
-                    write_ascii = .true.
-                    write_omega = .true.
-                    write_final_fields = .true.
-                    write_fields = .true.
-                    write_phi_over_time = .true.
-                    write_bpar_over_time = .true.
-                    write_apar_over_time = .true.
-                    write_nl_flux_dist = .true.
-                    write_fluxes = .true.
-                    write_nl_flux = .true.
-                    nwrite = 30
-                    navg = 10
-                    omegatol = 0.0001
-                    omegatinst = 500.0
-                    nsave = 5000
-                    save_for_restart = .true.
-                    write_final_epar = .true.
-                /
-
-                &parameters
-                    beta = 0.29798123734456977
-                    tite = 1.0
-                    zeff = 1.0
-                /
-
-                &diagnostics_config
-                    nwrite = 100000000
-                /
-            }}
-        """
-
-        # Write input to input file
-        with open("/opt/fast.in", "w") as f:
-            f.write(input)
-
-        # add MOOSE Python libraries to PYTHONPATH, modify input files and run achlys
-        os.system("sbatch some_script.sh") # submit to slurm or run directly?
+        input_file = "fast.in" # Select input file
+        dt = str(datetime.now()) # Creates timestamp unique to each run
+        dt = dt.split(" ")[0] + "_" + dt.split(" ")[1].replace(":", "-")
+        os.system(f"cd ~/gs2dock; mkdir {dt} && cp {input_file} ~/gs2-batch.sh {dt}") # Copy input file and SLURM script into the run folder
+        os.chdir(f"/home/mghw54/gs2dock/{dt}")
+        os.system("mkdir restart") # GS2 needs this folder otherwise will fail
+        with FileInput(files=input_file, inplace=True) as file:
+            checkpoint = 0
+            for line in file:
+                if "&species_parameters_3" in line:
+                    checkpoint += 1
+                if "tprim" in line and checkpoint == 1:
+                    line = f"    tprim = {parameters[0][0]}"
+                if "vnewk" in line and checkpoint == 1:
+                    line = f"    vnewk = {parameters[0][1]}"
+                print(line.rstrip()) # FileInput redirects stdout to file
         
-        # Read results CSV file, write rows to output
-        output = []
-        with open("/opt/achlys/problems/thermal_desorption/ogorodnikova/tds_multiapp/desorp_multi_out.csv", "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                output.append(row)
-                print(f"Read CSV Row: {row}")
-
-        # Probably no need for interpolation
-        # Interpolate data to a grid of size 500 before returning
-        from scipy.interpolate import interp1d
-        import pandas as pd
-        from numpy import linspace, ndarray
-
-        result = pd.read_csv('/opt/achlys/problems/thermal_desorption/ogorodnikova/tds_multiapp/desorp_multi_out.csv',usecols=['time','pfc_flux'])
-        f = interp1d(result['time'],result['pfc_flux'])
-        output = [ndarray.tolist(f(linspace(0,62.5,500)))]
-
-        print(f"output: {output}")
-
-        return output
+        # Run the model 
+        os.system(f"sbatch -W gs2-batch.sh {dt} {input_file}")
+        
+        # Read results using Pyrokinetics package and print output
+        os.chdir(f"/nobackup/mghw54/gs2runs/{dt}")
+        gs2_input = input_file
+        pyro = Pyro(gk_file=gs2_input, gk_code="GS2")
+        pyro.load_gk_output()
+        data = pyro.gk_output
+        print(data)
+        heat = data["heat"].sel(species="electron", field='phi').isel(time=-1)
+        growth_rate = data["growth_rate"].isel(time=-1)
+        mode_freq = data["mode_frequency"].isel(time=-1)
+        output = [[heat.to_numpy().squeeze().tolist(), growth_rate.to_numpy().squeeze().tolist(), mode_freq.to_numpy().squeeze().tolist()]]
+        return output 
 
     def supports_evaluate(self):
         return True
 
-model = AchlysModel()
+model = GS2Model()
 
-umbridge.serve_models([model], 4242)
+umbridge.serve_models([model], 4243)
