9b5c7b262d1671d1cb5a2902ce65d02f4567435e
Lorenzo Tamellini <tamellini@imati.cnr.it>
2023-11-29 18:12:06 +0100

    separating benchmark run and save from plot. Save results on txt and mat

--- benchmarks/cookies-problem/run_forward_benchmark_in_matlab.m
+++ benchmarks/cookies-problem/run_forward_benchmark_in_matlab.m
@@ -5,15 +5,21 @@
 
 %% preliminar operations:
 
-% 1) add sparse grids matlab kit and um-bridge matlab client to path
-addpath(genpath('~/GIT_projects/Github/sparse-grids-matlab-kit/')) % this is version 23.5 Robert
-addpath(genpath('~/GIT_projects/Github/umbridge/matlab/'))
+clear 
 
-% 2) run docker container with a command like
+% 1) run docker container with a command like
 % sudo docker run -it -p 4242:4242 <image name>
 
-clear 
- 
+
+% 2) add sparse grids matlab kit and um-bridge matlab client to path
+% addpath(genpath('~/GIT_projects/Github/sparse-grids-matlab-kit/')) % this is version 23.5 Robert
+% addpath(genpath('~/GIT_projects/Github/umbridge/matlab/'))
+
+
+% 3) to regenerate the files with the results (sparse grids in .txt and .mat, results in .txt and .mat), set the flag below to true
+saving_stuff = true;
+
+
 %% setup model 
 
 % specify model port
@@ -27,6 +33,9 @@ config = struct('NumThreads',4);
 % wrap model in an @-function too
 Psi_fun = @(y) model.evaluate(y',config);
 
+% a simple call to test that things are working fine
+% y_test = [-0.5; -0.5; -0.5; -0.5; -0.5; -0.5; -0.5; -0.5;];
+% Psi_fun(y_test)
 
 %% create sparse grid
 
@@ -36,7 +45,7 @@ N = 8;
 knots = @(n) knots_CC(n,-0.99,-0.2);
 lev2knots = @lev2knots_doubling;
 idxset_rule = @(i) sum(i-1);
-idxset_level_max = 5; % <--- controls max size of sparse grid
+idxset_level_max = 1; % <--- controls max size of sparse grid
 
 
 % With the choice above, the sparse grids will be nested. To recycle evaluations from one grid 
@@ -54,18 +63,29 @@ Psi_EV = []; % the expected value of Psi computed on each grid
 % here we go with the loop
 for w = 0:idxset_level_max
 
+    disp('========================================')
+    disp(strcat('w=',num2str(w)))
+    disp('========================================')
+
     % create sparse grid in extended format. Recycle some work from previous sparse grid
     S = create_sparse_grid(N,w,knots,lev2knots,idxset_rule,S_old);
 
     % reduce sparse grid
     Sr = reduce_sparse_grid(S);
-    
-    % save it to file
-    % grid_filename = strcat('sparse_grid_w=',num2str(w),'.txt');
-    % export_sparse_grid_to_file(Sr,grid_filename,'with_weights');
-    
+        
     % eval Psi on each point of sparse grid. Recycle available evaluations whenever possible
     Psi_evals = evaluate_on_sparse_grid(Psi_fun,S,Sr,Psi_evals_old,S_old,Sr_old);
+    
+    if saving_stuff
+        % save it to file. Type help export_sparse_grid_to_file for info on the saving format
+        grid_filename = strcat('sparse_grid_w=',num2str(w),'.txt');
+        export_sparse_grid_to_file(Sr,grid_filename,'with_weights');
+        % save also some minimal information in .mat format
+        grid_filename_mat = grid_filename(1:end-4); % i.e., remove .txt
+        save(grid_filename_mat,'S','Sr','Psi_evals')
+    end
+
+    
     
     % compute expected value and append the new value to previous container. Same for nb_pts
     Psi_EV(end+1) = quadrature_on_sparse_grid(Psi_evals,Sr);    %#ok<SAGROW>
@@ -79,16 +99,10 @@ for w = 0:idxset_level_max
 end
 
 
-% save values to file
-% save('cookies-benchmark-output.txt','nb_pts','Psi_EV','-ascii', '-double')
-
-%% plot convergence
-
-figure
-semilogx(nb_pts,Psi_EV,'-ok','LineWidth',2,'MarkerFaceColor','k')
-
-
-
-
-
-
+if saving_stuff
+    % save values to be plotted on file, in txt format. The data saved consists of the two vectors nb_pts and Psi_EV,
+    % they will be stored as rows of the txt file
+    save('cookies-benchmark-output.txt','nb_pts','Psi_EV','-ascii', '-double')
+    % save also on .mat
+    save('cookies-benchmark-output','nb_pts','Psi_EV')
+end
