f242322ebc061d901d1af1ddebe86b7a6ba546c8
nabr <nabr@dtu.dk>
2024-05-22 20:45:54 +0200

    Use Image2D without visual_only for forward model by creating functions to evaluate input image and output image in CUQI-CT

--- benchmarks/cuqi-ct/test_output.py
+++ benchmarks/cuqi-ct/test_output.py
@@ -41,12 +41,21 @@ dx = width/N
 
 Amat = dx*Amat
 
-# Create the CUQIpy linear model
-A = LinearModel(Amat)
+# Create Image2D geometries of image and sinogram spaces
+dg = Image2D(( N,N))
+rg = Image2D((nv,N))
 
-# Create the visual_only Image2D geometries of image and sinogram spaces
-dg = Image2D(( N,N), visual_only=True)
-rg = Image2D((nv,N), visual_only=True)
+# Define forward and adjoint operators (working on images)
+def forward(x):
+    y = Amat @ x.ravel()
+    return y.reshape((nv, N))
+
+def adjoint(y):
+    x = Amat.T @ y.ravel()
+    return x.reshape((N, N))
+
+# Create the CUQIpy linear model
+A = LinearModel(forward=forward, adjoint=adjoint, range_geometry=rg, domain_geometry=dg)
 
 # Equip linear operator with geometries
 A.domain_geometry = dg
