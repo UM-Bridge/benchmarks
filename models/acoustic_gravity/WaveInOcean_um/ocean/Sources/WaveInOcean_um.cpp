41ebc454d763bb69fe26f626c9c234ae41c1b11e
judubois <judubois@inria.fr>
2024-11-25 10:41:26 +0100

    input = three sources

--- models/acoustic_gravity/WaveInOcean_um/ocean/Sources/WaveInOcean_um.cpp
+++ models/acoustic_gravity/WaveInOcean_um/ocean/Sources/WaveInOcean_um.cpp
@@ -23,22 +23,19 @@ using namespace OndoMathX;
 class ExampleModel : public umbridge::Model {
 public:
 
-  ExampleModel(int test_delay) : umbridge::Model("forward"), 
-  test_delay(test_delay),
-  scaling(Lx, Lz), fespace(FEOrderX,FEOrderZ,Nx,Nz,scaling), 
-  phi(3), phi_hat(2), wx(2), 
-  Rho(_Rho), Rho_cm2(_Rho_cm2), scalar_to_vect_ez(_field_ez), 
-  cst_vec_ez(ez), cst_vec_ex(ex)
-  {
-  }
+  ExampleModel() 
+    : umbridge::Model("forward"),
+    scaling(Lx, Lz), fespace(FEOrderX,FEOrderZ,Nx,Nz,scaling), 
+    phi(3), phi_hat(2), wx(2), 
+    Rho(_Rho), Rho_cm2(_Rho_cm2), scalar_to_vect_ez(_field_ez), 
+    cst_vec_ez(ez), cst_vec_ex(ex)
+  {}
 
   void Init()
   {
-    Center[1] = Lz*Center[1];
     Index NDoFs = fespace.getNumDoFs();
 
     ////////// Get boundary spaces
-    auto bottom_fespace = fespace.getBndyFESpace(0);
     auto surface_fespace = fespace.getBndyFESpace(2);
 
     for (Index i = 0; i < nPointX; i++){ 
@@ -56,13 +53,12 @@ public:
     ///////////  Construct the matrices
     // The mass matrices are defined with weighted integration, the weight can be a scalar field or a constant 
     // e.g. CreateMass<1>(fespace, Rho_c2, massMatrixPhi_Sparse, 1.0); second parameter is the scalar field, last parameter is the constant. 
-    Field::Scalar unit_field(1.0); 
-
     //// Mass matrix for phi 
     CreateMass<1>(fespace, Rho_cm2, massMatrixPhi, 1.0); 
     AssembleMassBndy(surface_fespace, Field::IdentityField, massMatrixPhi, 1./delta2*_Rho({0,1,0})); 
 
     // Matrices for the pressure
+    Field::Scalar unit_field(1.0); 
     CreateMass<1>(fespace,unit_field,massMatrixUnit,1.0);
     ////////////////////////////////////////
 
@@ -81,60 +77,14 @@ public:
       
     ////////////////////////////////////////
     // Vectors
-    LAL::Vector sourceCoord, source;
     phi.Allocate(NDoFs);
     LAL::Allocate(pressure, NDoFs);
     LAL::Allocate(sourceVol, NDoFs); 
-    LAL::Allocate(sourceCoord, bottom_fespace.getNumDoFs());
-    LAL::Allocate(source, bottom_fespace.getNumDoFs());
-
-    // Compute f(x) at each DoF of the seabed 
-    LAL::SparseMatrix bottomMassMatrix;
-    CreateMassBndy(bottom_fespace, unit_field, bottomMassMatrix, _Rho({0,0,0}));
-    bottom_fespace.setAsFESpace();
-    for (Index i=0;i<bottom_fespace.getNumDoFs();++i)
-    {
-        Index dof = bottom_fespace._glob_bdny2glob_sq(i);
-        RealVector P;
-        // Get the (x,z) coordinates to evaluate the function sourceSpace
-        fespace.getDoFCoordinate(dof,P);
-        sourceCoord(i) = functionSpace(P) ;
-    }
-    // Get FE approximation of f(x) 
-    source = bottomMassMatrix * sourceCoord;
-    // Store the result in a vector of the size of the whole domain (NDoFs entries) to simplify the sum 
-    for (Index i=0;i<bottom_fespace.getNumDoFs();++i)
-    {
-        Index dof = bottom_fespace._glob_bdny2glob_sq(i);
-        sourceVol(dof) = source(i) ;
-    }
     ////////////////////////////////////////
 
     ///////////////// For PML /////////////
     phi_hat.Allocate(NDoFs);
     wx.Allocate(NDoFs);
-
-    /*
-    std::map<Index, Real> Dissip_map;
-    Dissip_map[0] = 0.0;
-    Dissip_map[1] = Dissip_coeff;
-    Field::Scalar<Field::Regularity::PiecewiseConstant> Dissip_Disc_Field(Dissip_map);
-    LAL::DiagonalMatrix massMatrixPhi_Dissip, massMatrixPhi_Bndy_Dissip;
-    auto subspace_pml = fespace.getSubDomainFESpace([](RealVector xyz){
-            if ((xyz[0] > Lx-LxPml_R) || (xyz[0] < LxPml_L))
-                return true;
-            else return false;
-    });
-    auto fespace_pml = subspace_pml;  
-    subspace_pml.setAsSubSpace(); 
-    fespace_pml.setAsFESpace(); 
-    CreateMass<1>(subspace_pml, Rho_cm2, massMatrixPhi_Dissip, Dissip_coeff);
-    CreateMassBndy(surface_fespace, Dissip_Disc_Field, massMatrixPhi_Bndy_Dissip, 1./delta2*rhoSurface);
-    massMatrix_p = massMatrixPhi + 0.5*dt*massMatrixPhi_Dissip + 0.5*dt*massMatrixPhi_Bndy_Dissip;
-    massMatrix_m = massMatrixPhi - 0.5*dt*massMatrixPhi_Dissip - 0.5*dt*massMatrixPhi_Bndy_Dissip;
-    CreateMassDG(fespace_pml, Field::IdentityField, massMatrix_wx,1.0);
-    */
-    //////////////////////////////////////
 
     ////////////////////////////////////////
     // Time and output 
@@ -144,7 +94,7 @@ public:
 
   // Define input and output dimensions of model (here we have a single vector of length 1 for input; same for output)
   std::vector<std::size_t> GetInputSizes(const json& config_json) const override {
-    return {1};
+    return {3};
   }
 
   std::vector<std::size_t> GetOutputSizes(const json& config_json) const override {
@@ -152,14 +102,11 @@ public:
   }
 
   std::vector<std::vector<double>> Evaluate(const std::vector<std::vector<double>>& inputs, json config) override {
-    // Do the actual model evaluation; here we just multiply the first entry of the first input vector by two, and store the result in the output.
-    // In addition, we support an artificial delay here, simulating actual work being done.
-    // std::this_thread::sleep_for(std::chrono::milliseconds(test_delay));
-    //return {{inputs[0][0] * 2.0}};
+    // Do the actual model evaluation
+    //Center[0] = inputs[0][0];
+    _sourceCenters = inputs[0];  
+    //_sourceWidths = inputs[0][1];
 
-    std::cout<< "test access fespace: " << fespace.getNumDoFs() << std::endl; 
-
-    Center[0] = Lx*inputs[0][0];
     std::vector<std::vector<double>> output(1);
     output[0] = {}; 
 
@@ -186,11 +133,43 @@ public:
     massMatrix_p = massMatrixPhi + 0.5*dt*massMatrixPhi_Dissip + 0.5*dt*massMatrixPhi_Bndy_Dissip;
     massMatrix_m = massMatrixPhi - 0.5*dt*massMatrixPhi_Dissip - 0.5*dt*massMatrixPhi_Bndy_Dissip;
     CreateMassDG(fespace_pml, Field::IdentityField, massMatrix_wx,1.0);
-    ////////////////////////////////////  
-    std::cout << "Running WaveInOcean........................" << std::endl;
+    //////////////////////////////////// 
 
-    for  (Index iStep = 0; iStep*dt < T_end; iStep++)
+
+    ////////// Create source here because it depends on the input
+    auto bottom_fespace = fespace.getBndyFESpace(0);
+    LAL::Vector sourceCoord, source;
+    LAL::Allocate(sourceCoord, bottom_fespace.getNumDoFs());
+    LAL::Allocate(source, bottom_fespace.getNumDoFs());
+    Field::Scalar unit_field(1.0); 
+
+    // Compute f(x) at each DoF of the seabed 
+    LAL::SparseMatrix bottomMassMatrix;
+    CreateMassBndy(bottom_fespace, unit_field, bottomMassMatrix, _Rho({0,0,0}));
+    bottom_fespace.setAsFESpace();
+    for (Index i=0;i<bottom_fespace.getNumDoFs();++i)
     {
+        Index dof = bottom_fespace._glob_bdny2glob_sq(i);
+        RealVector P;
+        // Get the (x,z) coordinates to evaluate the function sourceSpace
+        fespace.getDoFCoordinate(dof,P);
+        sourceCoord(i) = functionSpace(P) ;
+    }
+    // Get FE approximation of f(x) 
+    source = bottomMassMatrix * sourceCoord;
+    // Store the result in a vector of the size of the whole domain (NDoFs entries) to simplify the sum 
+    for (Index i=0;i<bottom_fespace.getNumDoFs();++i)
+    {
+        Index dof = bottom_fespace._glob_bdny2glob_sq(i);
+        sourceVol(dof) = source(i) ;
+    }
+    //////////////////////////////////
+
+    std::cout << "Running WaveInOcean........................" << std::endl;
+    
+    Index NOutput = 0; // used only for VTK
+    for  (Index iStep = 0; iStep*dt < T_end; iStep++)
+    {   
         // Operation  K_phi
         Core::MltAdd< Square<Scaling<2>>,Square<Scaling<2>>,true,true,true, Core::QuadratureOpt::Default,
             1,       //DimU
@@ -202,10 +181,10 @@ public:
             Core::DiffOp::Gradient
             >
             (fespace,fespace,Rho,-dt*dt,phi.getVector(1),0.0,phi.getVector(0)); 
-
+       
         // Add bottom source
         phi.getVector(0) += dt * dt * sourceVol * functionTime(dt*iStep);
-
+         
         // PML contributions 
         // Contribution of phi_hat
         Core::MltAdd<decltype(fespace_pml),decltype(subspace_pml),false,true,true, 
@@ -277,7 +256,7 @@ public:
         
         // Writing solution.
         if (iStep % OutputFreq == 0)
-        {   
+        { 
           // Get pressure, displacement formulation 
           Core::MltAdd< Square<Scaling<2>>,Square<Scaling<2>>,true,true,true, Core::QuadratureOpt::Default,
               1,       //DimU
@@ -294,14 +273,19 @@ public:
           LAL::Solve(massMatrixUnit, pressure);
           pressure += 1./(dt*dt) * (phi.getVector(0) - 2*phi.getVector(1) + phi.getVector(2));
           output[0].push_back( pressure(iObsPoint_vector[0]) ) ;
-        }
 
+          if( iStep % (NVtk * OutputFreq) == 0)
+          {
+           ParallelWriteVTK("../Data/Phi."+std::to_string(NOutput) + ".vtk", fespace, phi.getVector(0));
+           ParallelWriteVTK("../Data/P."+std::to_string(NOutput) + ".vtk", fespace, pressure);
+           NOutput++;
+          }
+        }
         // Swapping solutions.
         phi.Swap();
         phi_hat.Swap();
         wx.Swap();
     } 
-
     return output; 
   }
 
@@ -311,8 +295,6 @@ public:
   }
 
 private:
-  int test_delay;
-  
   Scaling<2> scaling;
   Square<Scaling<2>> fespace;
   //FESpaceTSubdomain< Square<Scaling<2>> ,2>* subspace_pml; //(Square<Scaling<2>> ,2> fespace, std::vector<Index> {0}); 
@@ -336,8 +318,30 @@ private:
   Field::Vector<2,Field::Regularity::Constant> cst_vec_ex;
   Field::ScalarToVector<2, Field::Regularity::C0> scalar_to_vect_ez; 
   
-  Real dt;
-  Index OutputFreq;
+  float dt;
+  int OutputFreq;
+
+  // parameters for the function f(x)
+  float spaceWidth {5}; 
+  float spaceFreq {10}; 
+
+  int _nSources = 3; 
+  // default: all sources at the center and 1 length unit (=1.5 km) long
+  std::vector<double> _sourceCenters {};  
+  std::vector<double> _sourceWidths {1,2,3} ;
+  int counter=0; 
+  Real functionSpace(const RealVector &p){
+    // Input: p = (x,z),  x \in [0,Lx], z \in [0,Lz]
+    Real result = 0;
+    
+    for (int i=0 ; i<_nSources ; i++)
+    { 
+      if (counter < 3) {std::cout << _sourceCenters[i] << " " << i << std::endl; }
+      counter ++; 
+      result += DoubleSigmoid(p[0], spaceFreq, _sourceCenters[i] - _sourceWidths[i]/2, _sourceWidths[i]);
+    }
+    return result;
+   }  
 
 };  
 
@@ -355,16 +359,16 @@ int main(){
     port = atoi(port_cstr);
   }
 
-  char const* delay_cstr = std::getenv("TEST_DELAY");
+  /*char const* delay_cstr = std::getenv("TEST_DELAY");
   int test_delay = 0;
   if ( delay_cstr != NULL ) {
     test_delay = atoi(delay_cstr);
   }
   std::cout << "Evaluation delay set to " << test_delay << " ms." << std::endl;
-
+  */
 
   // Set up and serve model
-  ExampleModel model(test_delay);
+  ExampleModel model;
   model.Init();
   umbridge::serveModels({&model}, "0.0.0.0", port);
 
