70e54a1bfe6311b0771bfd32857b12f245683ff4
dubois <dubois@tu-berlin.de>
2026-08-24 10:59:01 +0200

    Merge branch 'acoustic_gravity' of https://github.com/UM-Bridge/benchmarks into acoustic_gravity

--- models/acoustic_gravity/TerraDG/server.jl
+++ models/acoustic_gravity/TerraDG/server.jl
@@ -3,14 +3,17 @@ tempdir = mktempdir()
 Pkg.activate(tempdir)
 Pkg.add(["UMBridge"])
 using UMBridge
-TerraDG_path="/home/nxdj93/TerraDG.jl"
+# TO EDIT: Path to TerraDG on your machine, script can be executed from anywhere
+TerraDG_path="/home/areinarz/Desktop/TerraDG.jl"
 Pkg.develop(path=TerraDG_path) # here an absolute path to the TerraDG installation, you can run the file from anywhere
 using TerraDG
 
 const CONFIG_FILE = TerraDG_path * "/src/earthquake.yaml"
+const _base_config = TerraDG.Configuration(CONFIG_FILE)
+const N_OUTPUT_STEPS = floor(Int, (_base_config.end_time - _base_config.pressure_sensors_start) / _base_config.pressure_sensors_step) + 1
 
-function run_simulation(theta)
-    print("Julia threads available: ", Threads.nthreads())
+function run_simulation(theta, sensor_locations)
+    println("Julia threads available: ", Threads.nthreads())
     workdir = mktempdir()
 
     t0 = time()
@@ -25,17 +28,23 @@ function run_simulation(theta)
     elapsed = time() - t0
     print("Reading done in ", elapsed, ".\n Start simulation...")
 
-    # Run simulation, with explicit unique paths - no reliance on cwd,
-    # no shared files with any other concurrent call.
+    # Run simulation, with explicit unique paths
     t1 = time()
     output_prefix = joinpath(workdir, "output", "plot")
+    overrides = Dict(
+        "level_set_filename" => levelset_path,
+        "output" => Dict(
+            "pressure_sensors" => Dict(
+                "locations" => sensor_locations
+            )
+        )
+    )
     TerraDG.main(
-		         CONFIG_FILE;
-			         output_prefix=output_prefix,
-				         level_set_filename=levelset_path,
-					     )
-    elapsed = time() - t1
-    print("Simulation done in ", elapsed, ".\n Write output...")
+        CONFIG_FILE;
+        output_prefix=output_prefix,
+        config_overrides=overrides,
+    )
+    print("Simulation done in ", time() - t1, ".\n Write output...")
 
     # Read pressure sensor output
     t2 = time()
@@ -55,11 +64,17 @@ function run_simulation(theta)
     return output
 end
 
+
+function get_sensor_locations(config)
+    raw_locations = config["captors"]
+    return [(Float64(l[1]), Float64(l[2])) for l in raw_locations]
+end
+
 model = UMBridge.Model(
     name = "forward",
-    inputSizes = [100],
-    outputSizes = [202],
-    evaluate = (input, config) -> [run_simulation(input[1])]
+    inputSizes = [_base_config.grid_elements[1]],
+    outputSizes = (config) -> [length(get_sensor_locations(config)) * N_OUTPUT_STEPS],
+    evaluate = (input, config) -> [run_simulation(input[1], get_sensor_locations(config))]
 )
 
 UMBridge.serve_models([model], 4242)
